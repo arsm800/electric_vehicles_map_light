b7a7556b948574e906070ce293dc926d380793fc
Andrew Smith-Mui <arsm800@gmail.com>
2016-05-31 17:08:33 -0400

    Added link to project code to application.html.erb.  Created css margin adjustments in css file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ end
 require "csv"
 
 csv_text = File.read(Rails.root.join("db", "electric_fuel_stations.csv"))
-csv = CSV.parse(csv_text, :headers => true)
+csv = CSV.parse(csv_text, :headers => true, :encoding => "iso-8859-1:utf-8")
 csv.each do |row|
   e = Station.new
   e.city = row["city"]
